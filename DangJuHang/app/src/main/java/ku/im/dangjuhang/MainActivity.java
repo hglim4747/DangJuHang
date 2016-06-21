a747a85f9aaa8a46ea1ee33491dfe721373b968a
hglim4747 <hglim4747@naver.com>
2016-06-22 00:14:35 +0900

    t

--- DangJuHang/app/src/main/java/ku/im/dangjuhang/MainActivity.java
+++ DangJuHang/app/src/main/java/ku/im/dangjuhang/MainActivity.java
@@ -19,7 +19,6 @@ import android.widget.Toast;
 
 import ku.im.dangjuhang.Fragments.ArticleFragment;
 import ku.im.dangjuhang.Fragments.HeadlinesFragment;
-import ku.im.dangjuhang.Fragments.HeadlinesFragmentForRec;
 import ku.im.dangjuhang.Fragments.MyFrag;
 import ku.im.dangjuhang.Fragments.RegFrag;
 
